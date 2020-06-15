8df45409defefa4aafef574b5ab56c38bd3ff7e7
Tocci121 <65564047+Tocci121@users.noreply.github.com>
2020-06-15 19:00:30 +0800

    Update Hellogit.java

--- Hellogit/src/main/java/com/ming/git/Hellogit.java
+++ Hellogit/src/main/java/com/ming/git/Hellogit.java
@@ -10,8 +10,8 @@ public class Hellogit {
         System.out.println("更新1!");
         System.out.println("创建分支!");
         System.out.println("主干添加!");
-
         System.out.println("分支添加!");
+        System.out.println("在线更新!");
 
     }
 }
