d2547f7e31509e23aa09c768e72d77094ee55f62
Tocci121 <564706731@qq.com>
2020-06-15 17:37:42 +0800

    创建分支!

--- Hellogit/src/main/java/com/ming/git/Hellogit.java
+++ Hellogit/src/main/java/com/ming/git/Hellogit.java
@@ -8,5 +8,8 @@ public class Hellogit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("更新1!");
+        System.out.println("创建分支!");
+
+
     }
 }
