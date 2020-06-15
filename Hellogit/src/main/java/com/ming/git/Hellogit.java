3f8ac96471f0a7afd30160b912a012fb536179c8
Tocci121 <564706731@qq.com>
2020-06-15 17:23:09 +0800

    添加文件!

---
+++ Hellogit/src/main/java/com/ming/git/Hellogit.java
@@ -0,0 +1,11 @@
+package com.ming.git;
+
+/**
+ * @author luyu
+ * @create 2020-06-15 17:19
+ */
+public class Hellogit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
